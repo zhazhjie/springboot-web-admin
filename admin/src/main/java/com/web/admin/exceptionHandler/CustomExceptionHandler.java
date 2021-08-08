a6c66787af14e06717c9da7bbe3dd3a65165fcee
zhazhjie <583714409@qq.com>
2021-08-08 21:32:33 +0800

    feat:调整

--- admin/src/main/java/com/web/admin/exceptionHandler/CustomExceptionHandler.java
+++ admin/src/main/java/com/web/admin/exceptionHandler/CustomExceptionHandler.java
@@ -33,14 +33,14 @@ public class CustomExceptionHandler {
      * 业务异常
      */
     @ExceptionHandler(WebException.class)
-    @ResponseStatus(code= HttpStatus.BAD_REQUEST)
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
     public ResponseData handleWebException(WebException e) {
         log.error("业务异常", e);
         return ResponseData.fail(e.getCode(), e.getMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(code= HttpStatus.BAD_REQUEST)
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
     public ResponseData bindMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         FieldError fieldError = e.getBindingResult().getFieldError();
         if (null != fieldError) {
@@ -50,25 +50,25 @@ public class CustomExceptionHandler {
     }
 
     @ExceptionHandler(UnauthorizedException.class)
-    @ResponseStatus(code= HttpStatus.FORBIDDEN)
+    @ResponseStatus(code = HttpStatus.FORBIDDEN)
     public ResponseData handleUnauthorizedException(UnauthorizedException e) {
         return ResponseData.fail("没有权限");
     }
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    @ResponseStatus(code= HttpStatus.METHOD_NOT_ALLOWED)
+    @ResponseStatus(code = HttpStatus.METHOD_NOT_ALLOWED)
     public ResponseData handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
         return ResponseData.fail(String.format("不支持%s请求", e.getMethod()));
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    @ResponseStatus(code= HttpStatus.BAD_REQUEST)
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
     public ResponseData handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
         return ResponseData.fail(400, "请求参数类型不匹配");
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
-    @ResponseStatus(code= HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseData handleConstraintViolationException(ConstraintViolationException e) {
         Set<ConstraintViolation<?>> constraintViolations = e.getConstraintViolations();
         Iterator<ConstraintViolation<?>> iterator = constraintViolations.iterator();
